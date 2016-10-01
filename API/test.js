13116b7901409c32ebb3f64539583613e7d97789
benson <1275381@qq.com>
2016-10-01 09:24:49 -0700

    up

--- API/test.js
+++ API/test.js
@@ -42,9 +42,11 @@ function random(size) {
 app.all('/pull',function(req, res)
 //app.post('/compile',bruteforce.prevent,function(req, res)
 {
+    var exec = require('child_process').exec;
+    exec("echo xxx>/fc/compilebox/API/cccc.txt;docker pull "+config.imagename);
     console.log(req.body);
     res.send("ok");
-    
+
 
 });
 
